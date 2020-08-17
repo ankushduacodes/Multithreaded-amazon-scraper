9a334e4dd9172100fa249b5ec3f294a1ae375d69
ankushduacodes <61025943+ankushduacodes@users.noreply.github.com>
2020-08-17 16:22:56 +0530

    added example program to show ease of use

---
+++ example.py
@@ -0,0 +1,30 @@
+import time
+import argparse
+from amazon_scraper_module.scraper import Scraper
+
+
+def main():
+    """Takes command line argument
+    """
+    parser = argparse.ArgumentParser(
+        description='Extracts links contained in a url'
+    )
+    parser.add_argument(
+        '-w',
+        '--word',
+        nargs='?',
+        default='smart phone',
+        help='Enter the word you want to search'
+    )
+
+    arg = parser.parse_args()
+
+    amazon = Scraper()
+    amazon.search(arg.word)
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    main()
+    stop = time.perf_counter()
+    print(stop - start)
